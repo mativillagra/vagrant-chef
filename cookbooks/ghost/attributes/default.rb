67c9c7de919539ac93294bd10c47eeab4257f926
Matias Villagra <mativillagra@gmail.com>
2015-02-02 19:28:53 -0300

    correct typo on attributes ghost cookbook

--- cookbooks/ghost/attributes/default.rb
+++ cookbooks/ghost/attributes/default.rb
@@ -2,5 +2,5 @@ default[:ghost][:dir]       = "/home/vagrant/ghost"
 default[:ghost][:logs]      = "/home/vagrant/ghost/logs"
 default[:ghost][:user]      = "vagrant"
 default[:ghost][:group]     = "vagrant"
-default['ghost']['message'] = 'GHOST: glibc gethostbyname buffer overflow Qualys Security Advisory CVE-2015-0235'
-default['ghost']['url']     = 'http://www.cyberciti.biz/faq/cve-2015-0235-ghost-glibc-buffer-overflow-linux-test-program/'
+default[:ghost][:message]   = 'GHOST: glibc gethostbyname buffer overflow Qualys Security Advisory CVE-2015-0235'
+default[:ghost][:url]       = 'http://www.cyberciti.biz/faq/cve-2015-0235-ghost-glibc-buffer-overflow-linux-test-program/'
