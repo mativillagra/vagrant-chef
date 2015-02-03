f7ca18251af47a469ff448164f0a6bc9aeaf62cc
Matias Villagra <mativillagra@gmail.com>
2015-02-03 14:31:46 -0300

    modified & add cookbook to vagrant-ubuntu-role

--- roles/vagrant-ubuntu-role.rb
+++ roles/vagrant-ubuntu-role.rb
@@ -1,12 +1,13 @@
 # roles/vagrant-ubuntu-role.rb
 name "vagrant-ubuntu-role"
 description "Base role for Vagrant Ubuntu instances" 
-run_list "recipe[vim]", 
+run_list "recipe[apt]"
+         "recipe[vim]", 
          "recipe[curl]", 
          "recipe[apache2]",
          "recipe[git]",
          "recipe[motd]",
          "recipe[atri]",
          "recipe[ghost]",
-         "recipe[apt]"
+         "recipe[logrotate]"       
       
